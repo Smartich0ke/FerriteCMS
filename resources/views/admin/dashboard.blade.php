dced2f3fecb48d25f503adfa5b58427a44283a92
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-04-28 16:05:17 +0930

    refactor css class names and adjust some stuff

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,30 +3,30 @@
 
     <div class="d-flex flex-row justify-content-between w-100">
 
-        <div class="vh-100 p-5 dashboardContentBetweenSideBars">
-            <div class="d-flex flex-row quickMetricContainer w-100 mb-5 flex-wrap">
-                <div class="quickMetric d-flex flex-row gap-3 align-items-center bg-paleblue">
+        <div class="p-5 dashboard-subcontainer-main">
+            <div class="d-flex flex-row quick-metrics-container w-100 mb-5 flex-wrap">
+                <div class="quick-metric d-flex flex-row gap-3 align-items-center bg-paleblue">
                     <iconify-icon icon="mdi:graph-box" class="p-0" width="64" height="64"></iconify-icon>
                     <div class="">
                         <h3 class="mb-0 text-center"><strong>500k</strong></h3>
                         <div class="my-0 text-center">users</div>
                     </div>
                 </div>
-                <div class="quickMetric d-flex flex-row gap-3 align-items-center bg-palered">
+                <div class="quick-metric d-flex flex-row gap-3 align-items-center bg-palered">
                     <iconify-icon icon="mdi:heart" class="p-0" width="64" height="64"></iconify-icon>
                     <div class="">
                         <h3 class="mb-0 text-center"><strong>620</strong></h3>
                         <div class="my-0 text-center">Likes</div>
                     </div>
                 </div>
-                <div class="quickMetric d-flex flex-row gap-3 align-items-center bg-palegreen">
+                <div class="quick-metric d-flex flex-row gap-3 align-items-center bg-palegreen">
                     <iconify-icon icon="mdi:users-group" class="p-0" width="64" height="64"></iconify-icon>
                     <div class="">
                         <h3 class="mb-0 text-center"><strong>318</strong></h3>
                         <div class="my-0 text-center">Following</div>
                     </div>
                 </div>
-                <div class="quickMetric d-flex flex-row gap-3 align-items-center bg-paleyellow">
+                <div class="quick-metric d-flex flex-row gap-3 align-items-center bg-paleyellow">
                     <iconify-icon icon="mdi:note-multiple" class="p-0" width="64" height="64"></iconify-icon>
                     <div class="">
                         <h3 class="mb-0 text-center"><strong>103</strong></h3>
@@ -39,13 +39,13 @@
             <div class="d-flex flex-row justify-content-between w-100">
 
                 <!-- Visitor metrics -->
-                <div style="" class="me-4 visitorMetrics pb-5">
+                <div style="" class="me-4 metrics-visitor pb-5">
                     <h2>Site Visitors (Last 7 Days)</h2>
                     <active-users-chart></active-users-chart>
                 </div>
 
                 <!-- Browser metrics -->
-                <div class="browserMetrics">
+                <div class="metrics-browser">
                     <h2>Browsers</h2>
                     <browsers-chart></browsers-chart>
                 </div>
@@ -134,7 +134,7 @@
         </div>
 
         <!-- Sidebar content -->
-        <aside class="vh-100 p-5 dashboardAside">
+        <aside class="p-5 dashboard-subcontainer-aside">
 
             <!-- Recent comments -->
             <div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,125 +21,129 @@
 </head>
 <body>
 <div id="app">
-    <div class="d-flex flex-row justify-content-start adminContainer">
-        <nav class="adminNav" style="width: 20%">
+    <div class="d-flex flex-row justify-content-start admin-panel">
 
-            <div class="d-flex flex-column flex-shrink-0 text-white bg-dark sideBar" >
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none d-flex align-items-center gap-2">
-                    <iconify-icon icon="mdi:cog-box" width="32" height="32"></iconify-icon>
-                    <span class="fs-4">Admin Panel</span>
-                </a>
-                <hr>
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="">
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2" aria-current="page">
-                            <iconify-icon icon="material-symbols:home" width="16" height="16"></iconify-icon>
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2 active">
-                            <iconify-icon icon="mdi:view-dashboard" width="16" height="16"></iconify-icon>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
-                            <iconify-icon icon="mdi:note-text" width="16" height="16"></iconify-icon>
-                            Posts
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
-                            <iconify-icon icon="mdi:comment-text-multiple" width="16" height="16"></iconify-icon>
-                            Comments
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
-                            <iconify-icon icon="mdi:image-multiple" width="16" height="16"></iconify-icon>
-                            Images
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
-                            <iconify-icon icon="mdi:file-multiple" width="16" height="16"></iconify-icon>
-                            File Storage
-                        </a>
-                    </li>
-                </ul>
-                <hr>
-                <div class="dropdown">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/Smartich0ke.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                        <strong>{{ auth()->user()->name }}</strong>
+        <nav class="d-flex flex-column flex-shrink-0 text-white bg-dark sideBar admin-nav">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none d-flex align-items-center gap-2">
+                <iconify-icon icon="mdi:cog-box" width="32" height="32"></iconify-icon>
+                <span class="fs-4">Admin Panel</span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="">
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2" aria-current="page">
+                        <iconify-icon icon="material-symbols:home" width="16" height="16"></iconify-icon>
+                        Home
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="#">New Post...</a></li>
-                        <li><a class="dropdown-item" href="#">New Gallery Image...</a></li>
-                        <li><a class="dropdown-item" href="#">Profile Settings</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2 active">
+                        <iconify-icon icon="mdi:view-dashboard" width="16" height="16"></iconify-icon>
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
+                        <iconify-icon icon="mdi:note-text" width="16" height="16"></iconify-icon>
+                        Posts
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
+                        <iconify-icon icon="mdi:comment-text-multiple" width="16" height="16"></iconify-icon>
+                        Comments
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
+                        <iconify-icon icon="mdi:image-multiple" width="16" height="16"></iconify-icon>
+                        Images
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white d-flex flex-row align-items-center gap-2">
+                        <iconify-icon icon="mdi:file-multiple" width="16" height="16"></iconify-icon>
+                        File Storage
+                    </a>
+                </li>
+            </ul>
+            <hr>
+            <div class="dropdown">
+                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/Smartich0ke.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <strong>{{ auth()->user()->name }}</strong>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                    <li><a class="dropdown-item" href="#">New Post...</a></li>
+                    <li><a class="dropdown-item" href="#">New Gallery Image...</a></li>
+                    <li><a class="dropdown-item" href="#">Profile Settings</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
             </div>
         </nav>
 
         <!-- Nav buffer -->
-        <div class="navBuffer"></div>
+        <div class="nav-buffer"></div>
 
         <!-- Mini Nav -->
-            <nav class="adminNav-mini d-flex flex-column flex-shrink-0 bg-dark">
-                <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
-                    <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-                    <span class="visually-hidden">Icon-only</span>
-                </a>
-                <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active py-3 " aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="material-symbols:home" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 " title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="mdi:view-dashboard" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 " title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="mdi:note-text" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 " title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="mdi:comment-text-multiple" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 " title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="mdi:image-multiple" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 " title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                            <iconify-icon icon="mdi:file-multiple" width="24" height="24"></iconify-icon>
-                        </a>
-                    </li>
-                </ul>
-                <div class="dropdown border-top">
-                    <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
+        <nav class="admin-nav-mini d-flex flex-column flex-shrink-0 bg-dark">
+            <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
+                <svg class="bi" width="40" height="32">
+                    <use xlink:href="#bootstrap"/>
+                </svg>
+                <span class="visually-hidden">Icon-only</span>
+            </a>
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active py-3 " aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="material-symbols:home" width="24" height="24"></iconify-icon>
                     </a>
-                    <ul class="dropdown-menu-dark dropdown-menu text-small shadow bg-dark " aria-labelledby="dropdownUser3">
-                        <li><a class="dropdown-item " href="#">New project...</a></li>
-                        <li><a class="dropdown-item " href="#">Settings</a></li>
-                        <li><a class="dropdown-item " href="#">Profile</a></li>
-                        <li><hr class="dropdown-divider "></li>
-                        <li><a class="dropdown-item " href="#">Sign out</a></li>
-                    </ul>
-                </div>
-            </nav>
-        <main class="p-0 adminContentMain">
+                </li>
+                <li>
+                    <a href="#" class="nav-link py-3 " title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="mdi:view-dashboard" width="24" height="24"></iconify-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link py-3 " title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="mdi:note-text" width="24" height="24"></iconify-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link py-3 " title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="mdi:comment-text-multiple" width="24" height="24"></iconify-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link py-3 " title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="mdi:image-multiple" width="24" height="24"></iconify-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link py-3 " title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
+                        <iconify-icon icon="mdi:file-multiple" width="24" height="24"></iconify-icon>
+                    </a>
+                </li>
+            </ul>
+            <div class="dropdown border-top">
+                <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu-dark dropdown-menu text-small shadow bg-dark " aria-labelledby="dropdownUser3">
+                    <li><a class="dropdown-item " href="#">New project...</a></li>
+                    <li><a class="dropdown-item " href="#">Settings</a></li>
+                    <li><a class="dropdown-item " href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider ">
+                    </li>
+                    <li><a class="dropdown-item " href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </nav>
+        <main class="p-0 admin-content">
             @yield('content')
         </main>
     </div>
