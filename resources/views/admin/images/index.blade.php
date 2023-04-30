031f9a867f055ee3e49b95f0eecba649f3c1db3c
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-04-30 14:57:28 +0930

    add image index view

--- resources/views/admin/images/index.blade.php
+++ resources/views/admin/images/index.blade.php
@@ -1,4 +1,68 @@
 @extends('layouts.admin')
 @section('content')
-    <div>admin.images.index</div>
+    <div class="m-5">
+        <h1>All Images</h1>
+
+        <div class="">
+            <table class="table table-responsive">
+                <thead>
+                <tr>
+                    <th scope="col" >ID</th>
+                    <th scope="col" >Alt Text</th>
+                    <th scope="col" >Likes</th>
+                    <th scope="col" >Created</th>
+
+                    <th scope="col" >Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="my-5">
+                    <th scope="row">1234</th>
+                    <td>Some alt text</td>
+                    <td>50</td>
+                    <td>2021-01-01</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary me-1">View</button>
+                        <button class="btn btn-sm btn-outline-primary me-1">Edit</button>
+                        <button class="btn btn-sm btn-outline-danger">Move To Rubbish Bin</button>
+                    </td>
+                </tr>
+                <tr class="my-5">
+                    <th scope="row">1234</th>
+                    <td>Some alt text</td>
+                    <td>50</td>
+                    <td>2021-01-01</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary me-1">View</button>
+                        <button class="btn btn-sm btn-outline-primary me-1">Edit</button>
+                        <button class="btn btn-sm btn-outline-danger">Move To Rubbish Bin</button>
+                    </td>
+                </tr>
+                <tr class="my-5">
+                    <th scope="row">1234</th>
+                    <td>Some alt text</td>
+                    <td>50</td>
+                    <td>2021-01-01</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary me-1">View</button>
+                        <button class="btn btn-sm btn-outline-primary me-1">Edit</button>
+                        <button class="btn btn-sm btn-outline-danger">Move To Rubbish Bin</button>
+                    </td>
+                </tr>
+                <tr class="my-5">
+                    <th scope="row">1234</th>
+                    <td>Some alt text</td>
+                    <td>50</td>
+                    <td>2021-01-01</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-primary me-1">View</button>
+                        <button class="btn btn-sm btn-outline-primary me-1">Edit</button>
+                        <button class="btn btn-sm btn-outline-danger">Move To Rubbish Bin</button>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
