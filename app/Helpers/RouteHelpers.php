5604fc169e561f94f62dad2d5b1ca5165f66d98d
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-05-03 20:21:28 +0930

    add post url helper

--- app/Helpers/RouteHelpers.php
+++ app/Helpers/RouteHelpers.php
@@ -4,3 +4,7 @@ function isActiveRoute($routePattern)
 {
     return fnmatch($routePattern, Route::currentRouteName());
 }
+function postURL($post)
+{
+    return route('posts.show', $post->slug);
+}
