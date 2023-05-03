e118d1f7ffaedd99b462819aaa1a008ef2519736
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-05-04 08:14:21 +0930

    add post user relationship

--- database/migrations/2023_04_30_052648_create_posts_table.php
+++ database/migrations/2023_04_30_052648_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->longText('excerpt');
             $table->longText('body');
