09fccfadea7b8d548e7e7c8df89ed323cb8de773
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-05-04 16:45:40 +0930

    add category to posts relationship

--- database/migrations/2023_04_30_052648_create_posts_table.php
+++ database/migrations/2023_04_30_052648_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignid('category_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->longText('excerpt');
             $table->longText('body');
