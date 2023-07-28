bebf62222e9775d75b549f2939680c81cea132d0
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-07-29 08:36:20 +0930

    add message if no content exists

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -15,6 +15,11 @@
                     "
                      style="background-image: url({{ Storage::url($image->image)}});"></div>
             @endforeach
+            @if($images->count() == 0)
+                <div class="d-flex flex-row justify-content-center">
+                    <div class="text-muted mt-5">Looks like there's not much here at the moment.</div>
+                </div>
+            @endif
         </div>
         <div class="text-sm text-muted text-center m-3">See more on my Instagram: <a href="#" class="link">@npatrick_photos</a></div>
     </article>
