4de30a402dd36cc179a01d4be8b56cb630df46a9
Smartich0ke <nikolaipatricknp@gmail.com>
2024-03-03 15:51:17 +1030

    update socials

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -4,11 +4,12 @@
         <div class="d-flex flex-column gap-1 justify-content-center align-items-center">
             <h1 class="display-4 text-dark text-center">Nikolai Patrick</h1>
             <h2 class="text-center">Software development, engineering, photography</h2>
-            <div class="d-flex flex-row gap-2 justify-content-center align-items-center">
-                <a href="https://www.instagram.com/npatrick_photos/" class="text-dark" ><iconify-icon height="48" width="48" icon="mdi:instagram"></iconify-icon></a>
-                <a href="https://github.com/Smartich0ke" class="text-dark" ><iconify-icon height="48" width="48" icon="mdi:github"></iconify-icon></a>
-                <a href="https://youtube.com/@Artichoked1" class="text-dark" ><iconify-icon height="48" width="48" icon="mdi:youtube"></iconify-icon></a>
-                <a href="" onclick="alert('Discord handle: artichoked_')" class="text-dark" ><iconify-icon height="48" width="48" icon="mdi:discord"></iconify-icon></a>
+            <div class="d-flex flex-row gap-3 justify-content-center align-items-center">
+                <a href="https://www.instagram.com/npatrick_photos/" class="text-dark" ><iconify-icon height="48" width="48" icon="simple-icons:instagram"></iconify-icon></a>
+                <a href="https://github.com/Smartich0ke" class="text-dark" ><iconify-icon height="48" width="48" icon="simple-icons:github"></iconify-icon></a>
+                <a href="https://mastodon.artichokenetwork.com/@artichoke" class="text-dark" ><iconify-icon height="48" width="48" icon="simple-icons:mastodon"></iconify-icon></a>
+                <a href="https://matrix.to/#/@artichoke:artichokenetwork.com" class="text-dark" ><iconify-icon height="48" width="48" icon="simple-icons:element"></iconify-icon></a>
+                <a href="" onclick="alert('Discord handle: artichoked_')" class="text-dark" ><iconify-icon height="48" width="48" icon="simple-icons:discord"></iconify-icon></a>
             </div>
         </div>
     </header>
